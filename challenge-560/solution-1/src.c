ca398d8d1fb054a1f2d19784e359627e97f75dd2
S2ealea <tgnaugz@jxnu.edu.cn>
2021-08-22 16:33:19 +0800

    update 20210822

---
+++ challenge-560/solution-1/src.c
@@ -0,0 +1,49 @@
+/* Brute force algorithme
+* Not Accepted.
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	
+
+/*
+* Input1: array
+* Input2: array size
+* Input3: sum k
+*
+* Output: nums of the subarrays which 
+* sum of it's elements equals k.
+*/
+
+
+int subarraySum(int* nums, int numsSize, int k){
+    int count = 0; // Number of the subarrays
+    int start = 0;
+    int end = 0;
+
+
+	for( start = 0; start < numsSize; ++start )
+	{
+		int sum = 0;
+		for ( end = start; end < numsSize; )
+		{
+			sum += nums[end];
+			printf("%d\n",sum );
+			if ( sum == k )
+				++count;
+			++end;
+		}
+
+	}
+	return count;
+
+}
+
+    // Test code
+    int array[] = {1,3,3,4,3,5,6,34,-27};
+    printf("%d\n",subarraySum(array,9,7));
+
+	return 0;
+}
